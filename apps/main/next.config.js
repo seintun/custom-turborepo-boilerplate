7a3002ad802aee8b0176dc95ad816c50199b4114
Sein Tun <contact@seintun.dev>
2023-04-23 23:33:54 -0700

    feat(cicd): Integrate lint, prettier, husky, changeset, and misc configs

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    typedRoutes: true,
-  },
+    experimental: {
+        appDir: true,
+        typedRoutes: true,
+    },
 };
 
 module.exports = nextConfig;
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,21 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+    extends: ['next', 'turbo', 'prettier'],
+    rules: {
+        '@next/next/no-html-link-for-pages': 'off',
     },
-  },
+    parserOptions: {
+        babelOptions: {
+            presets: [require.resolve('next/babel')],
+        },
+    },
+    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
+    rules: {
+        'react-hooks/exhaustive-deps': 'error',
+        'no-var': 'error',
+        'brace-style': 'error',
+        'prefer-template': 'error',
+        radix: 'error',
+        'space-before-blocks': 'error',
+        'import/prefer-default-export': 'off',
+    },
 };
